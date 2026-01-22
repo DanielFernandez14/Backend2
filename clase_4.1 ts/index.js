e6e52756e7b057a7437bf5be3864356089627fc3
DanoDev <daniel.mfernandez1999@gmail.com>
2026-01-21 22:34:25 -0300

    clase_4 parte 2

---
+++ clase_4.1 ts/index.js
@@ -0,0 +1,4 @@
+const sumar = (n1, n2) => {
+    console.log(n1 + n2)
+}
+sumar("1000", 2) /// lo suma el number a string -> infiere en el sentido semantico, no deberia convertir el numero de number a string
---
+++ clase_4.1 ts/index.ts
@@ -0,0 +1,6 @@
+const sumar = (n1: number, n2: number) => {
+    console.log(n1 + n2)
+}
+
+sumar(1, 2) /// lo suma el number a string -> infiere en el sentido semantico, no deberia convertir el numero de number a string
+export  { sumar }

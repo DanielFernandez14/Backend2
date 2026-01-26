9a93e4f86edf14c3cd55c829edd0df0d8b428150
DanoDev <daniel.mfernandez1999@gmail.com>
2026-01-26 20:30:28 -0300

    incorporando conexión a mongodb

--- clase_11/src/index.ts
+++ clase_11/src/index.ts
@@ -1,1 +1,30 @@
-console.log("hola")
+import mongoose from 'mongoose'
+import { Schema, model, connect } from 'mongoose'
+
+process.loadEnvFile()
+
+const URI_DB = process.env.URI_DB || ""
+
+
+const connectMongoDB = async () =>  {
+    try {
+        await connect(URI_DB)
+        console.log("Conectado con exito a MongoDB ✅")
+    } catch (error) {
+        console.log("Error al conectar a MongoDB ❌")
+    }
+}
+
+const filmSchema = new Schema ({
+    title: {type: String, required: true, unique: true},
+    year: {type: Number, required: true},
+    rating: {type: Number, required: true},
+    gender: {type: String, required: true}
+})
+
+const File = model("film", filmSchema)
+
+
+
+connectMongoDB()
+
